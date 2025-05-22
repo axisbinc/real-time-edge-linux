b9c41080d123bf4e53d4df379f964eec51800485
Praveen Yaramada <praveen@axisbinc.com>
2025-05-22 01:41:39 -0700

    set_tdm_slot operation in pcm1690

--- sound/soc/codecs/pcm1690.c
+++ sound/soc/codecs/pcm1690.c
@@ -25,14 +25,31 @@
 #define pcm1690_DAC_VOL_LEFT	0x4A
 #define pcm1690_DAC_VOL_RIGHT	0x4B
 
-#define pcm1690_FMT_MASK	0x07
+#define pcm1690_FMT_MASK	0x0F
 #define pcm1690_MUTE_MASK	0x03
 #define pcm1690_MUTE_SRET	0x06
 
+#define PCM1690_FMT_I2S		0x0
+#define PCM1690_FMT_LEFT_J		0x1
+#define PCM1690_FMT_RIGHT_J		0x2
+#define PCM1690_FMT_RIGHT_J_16		0x3
+#define PCM1690_FMT_DSP_A		0x4
+#define PCM1690_FMT_DSP_B		0x5
+#define PCM1690_FMT_I2S_TDM		0x6
+#define PCM1690_FMT_LEFT_J_TDM		0x7
+
+struct pcm1690_io_params {
+	unsigned int format;
+	int tdm_slots;
+	u32 tdm_mask;
+	int slot_width;
+	int pcm_width;
+	unsigned int rate;
+};
+
 struct pcm1690_private {
 	struct regmap *regmap;
-	unsigned int format;
-	unsigned int rate;
+	struct pcm1690_io_params io_params;
 	struct gpio_desc *reset;
 	struct work_struct work;
 	struct device *dev;
@@ -56,14 +73,14 @@ static bool pcm1690_writeable_reg(struct device *dev, unsigned int reg)
 }
 
 static int pcm1690_set_dai_fmt(struct snd_soc_dai *codec_dai,
-			       unsigned int format)
+				   unsigned int format)
 {
 	struct snd_soc_component *component = codec_dai->component;
 	struct pcm1690_private *priv = snd_soc_component_get_drvdata(component);
 
-	priv->format = format;
+	priv->io_params.format = format;
 
-    dev_info(priv->dev, "pcm1690: format %d\n", format);
+	dev_info(priv->dev, "pcm1690: format %d\n", format);
 
 	return 0;
 }
@@ -78,58 +95,51 @@ static int pcm1690_mute(struct snd_soc_dai *codec_dai, int mute, int direction)
 				  mute ? 0 : pcm1690_MUTE_MASK);
 }
 
+static int pcm1690_set_dai_tdm_slot(struct snd_soc_dai *dai,
+					unsigned int tx_mask,
+					unsigned int rx_mask,
+					int slots, int slot_width)
+{
+	struct snd_soc_component *component = dai->component;
+	struct pcm1690_private *priv = snd_soc_component_get_drvdata(component);
+	int ret;
+
+	priv->io_params.tdm_slots = slots;
+	priv->io_params.tdm_mask = tx_mask;
+	priv->io_params.slot_width = slot_width;
+	dev_info(priv->dev, "pcm1690: tx_mask %x, rx_mask %x, slots %d, slot_width %d\n",
+		 tx_mask, rx_mask, slots, slot_width);
+
+	return 0;
+}
+
 static int pcm1690_hw_params(struct snd_pcm_substream *substream,
-			     struct snd_pcm_hw_params *params,
-			     struct snd_soc_dai *codec_dai)
+				 struct snd_pcm_hw_params *params,
+				 struct snd_soc_dai *codec_dai)
 {
 	struct snd_soc_component *component = codec_dai->component;
 	struct pcm1690_private *priv = snd_soc_component_get_drvdata(component);
 	int val = 0, ret;
 
-	priv->rate = params_rate(params);
+	priv->io_params.rate = params_rate(params);
+	priv->io_params.pcm_width = params_width(params);
 
-	switch (priv->format & SND_SOC_DAIFMT_FORMAT_MASK) {
-	case SND_SOC_DAIFMT_RIGHT_J:
-		switch (params_width(params)) {
-		case 24:
-			val = 2;
-			break;
-		case 16:
-			val = 3;
-			break;
-		default:
-			return -EINVAL;
-		}
-		break;
-	case SND_SOC_DAIFMT_I2S:
-		switch (params_width(params)) {
-		case 16:
-		case 24:
-		case 32:
-			val = 0;
-			break;
-		default:
-			return -EINVAL;
-		}
-		break;
+	switch (priv->io_params.format & SND_SOC_DAIFMT_FORMAT_MASK) {
 	case SND_SOC_DAIFMT_LEFT_J:
-		switch (params_width(params)) {
-		case 16:
-		case 24:
-		case 32:
-			val = 1;
-			break;
-		default:
-			return -EINVAL;
+		if (priv->io_params.tmd_slots > 2) {
+			val = PCM1690_FMT_LEFT_J_TDM;
+		} else {
+			val = PCM1690_FMT_LEFT_J;
 		}
 		break;
+	/* TODO: revisit to handle other formats */
 	default:
 		dev_err(component->dev, "Invalid DAI format\n");
 		return -EINVAL;
 	}
 
-    dev_info(priv->dev, "pcm1690: format %d, rate %d, width %d\n", priv->format,
-         priv->rate, params_width(params));
+	dev_info(priv->dev, "pcm1690: format %d, rate %d, width %d\n", priv->format,
+		 priv->rate, params_width(params));
 	ret = regmap_update_bits(priv->regmap, pcm1690_FMT_CONTROL,
 				 pcm1690_FMT_MASK, val);
 	if (ret < 0)
@@ -141,12 +151,12 @@ static int pcm1690_hw_params(struct snd_pcm_substream *substream,
 static void pcm1690_work_queue(struct work_struct *work)
 {
 	struct pcm1690_private *priv = container_of(work,
-						    struct pcm1690_private,
-						    work);
+							struct pcm1690_private,
+							work);
 
 	/* Perform a software reset to remove codec from desynchronized state */
 	if (regmap_update_bits(priv->regmap, pcm1690_MUTE_CONTROL,
-			       0x3 << pcm1690_MUTE_SRET, 0) < 0)
+				   0x3 << pcm1690_MUTE_SRET, 0) < 0)
 		dev_err(priv->dev, "Error while setting SRET");
 }
 
@@ -178,6 +188,7 @@ static const struct snd_soc_dai_ops pcm1690_dai_ops = {
 	.set_fmt	= pcm1690_set_dai_fmt,
 	.hw_params	= pcm1690_hw_params,
 	.mute_stream	= pcm1690_mute,
+	.set_tdm_slot = pcm1690_set_dai_tdm_slot,
 	.trigger	= pcm1690_trigger,
 	.no_capture_mute = 1,
 };
@@ -186,8 +197,8 @@ static const DECLARE_TLV_DB_SCALE(pcm1690_dac_tlv, -12000, 50, 1);
 
 static const struct snd_kcontrol_new pcm1690_controls[] = {
 	SOC_DOUBLE_R_RANGE_TLV("DAC Playback Volume", pcm1690_DAC_VOL_LEFT,
-			       pcm1690_DAC_VOL_RIGHT, 0, 0xf, 0xff, 0,
-			       pcm1690_dac_tlv),
+				   pcm1690_DAC_VOL_RIGHT, 0, 0xf, 0xff, 0,
+				   pcm1690_dac_tlv),
 };
 
 static const struct snd_soc_dapm_widget pcm1690_dapm_widgets[] = {
@@ -246,7 +257,7 @@ int pcm1690_common_init(struct device *dev, struct regmap *regmap)
 	struct pcm1690_private *pcm1690;
 
 	pcm1690 = devm_kzalloc(dev, sizeof(struct pcm1690_private),
-			       GFP_KERNEL);
+				   GFP_KERNEL);
 	if (!pcm1690)
 		return -ENOMEM;
 
@@ -264,7 +275,7 @@ int pcm1690_common_init(struct device *dev, struct regmap *regmap)
 	INIT_WORK(&pcm1690->work, pcm1690_work_queue);
 
 	return devm_snd_soc_register_component(dev, &soc_component_dev_pcm1690,
-					       &pcm1690_dai, 1);
+						   &pcm1690_dai, 1);
 }
 EXPORT_SYMBOL_GPL(pcm1690_common_init);
 
