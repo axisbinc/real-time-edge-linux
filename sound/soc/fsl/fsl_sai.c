c0e4af99e0989c7c737783a5d595b2f4ca3e527c
Praveen Yaramada <praveen@axisbinc.com>
2025-05-30 01:43:01 -0700

    debug messages on fsl_sai.c

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -544,6 +544,13 @@ static int fsl_sai_hw_params(struct snd_pcm_substream *substream,
 	u32 watermark;
 	int ret, i;
 
+    dev_info(cpu_dai->dev,
+        "SAI hw_params: ch=%u rate=%u slots=%u slot_width=%u\n",
+        params_channels(params),
+        params_rate(params),
+        sai->slots,
+        sai->slot_width);
+
 	if (sai->slot_width)
 		slot_width = sai->slot_width;
 
@@ -762,6 +769,8 @@ static int fsl_sai_trigger(struct snd_pcm_substream *substream, int cmd,
 	int dir = tx ? TX : RX;
 	u32 xcsr;
 
+    dev_info(cpu_dai->dev, "SAI trigger: cmd=%d\n", cmd);
+
 	/*
 	 * Asynchronous mode: Clear SYNC for both Tx and Rx.
 	 * Rx sync with Tx clocks: Clear SYNC for Tx, set it for Rx.
@@ -846,6 +855,8 @@ static int fsl_sai_startup(struct snd_pcm_substream *substream,
 	int ret, i, j, k = 0;
 	u64 clk_rate[2];
 
+    dev_info(cpu_dai->dev, "SAI: startup for %s\n",
+        substream->stream == SNDRV_PCM_STREAM_PLAYBACK ? "playback" : "capture");
 	/*
 	 * EDMA controller needs period size to be a multiple of
 	 * tx/rx maxburst
