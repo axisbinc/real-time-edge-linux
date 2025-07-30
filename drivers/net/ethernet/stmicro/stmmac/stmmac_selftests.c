a9a2b6712416b9161937c889bad364209e8d9c1a
Anuj <anuj@axisbinc.com>
2025-07-30 11:47:15 +0000

    Changed key values to correctly match the ip protocol. Fixed some formatting issues.

--- drivers/net/ethernet/stmicro/stmmac/stmmac_selftests.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_selftests.c
@@ -214,6 +214,10 @@ struct sk_buff *stmmac_test_get_udp_skb(struct stmmac_priv *priv,
 	if (attr->timestamp)
 		skb->tstamp = ns_to_ktime(attr->timestamp);
 
+	pr_info("Printing from get skb function.\n");
+	ehdr = (struct ethhdr *)skb_mac_header(skb);
+	print_hex_dump(KERN_INFO, "EHDR: ", DUMP_PREFIX_OFFSET, 16, 1, 
+               ehdr, 16, true);
 	return skb;
 }
 
@@ -1084,11 +1088,11 @@ static int stmmac_test_dvlanfilt_perfect(struct stmmac_priv *priv)
 #ifdef CONFIG_NET_CLS_ACT
 
 static int stmmac_test_rxp_setup_new(
-    struct tc_cls_u32_offload *cls_u32, __be16 val, int offset, int tcfa)
+    struct tc_cls_u32_offload *cls_u32, __be32 val, int offset, int tcfa)
 {
-    struct tc_u32_sel *sel;
+	struct tc_u32_sel *sel;
 	struct tcf_exts *exts;
-    struct tcf_gact *gact;
+	struct tcf_gact *gact;
 
 	cls_u32->command = TC_CLSU32_NEW_KNODE;
 	cls_u32->common.chain_index = 0;
@@ -1097,16 +1101,16 @@ static int stmmac_test_rxp_setup_new(
 	cls_u32->knode.sel = NULL;
 	cls_u32->knode.handle = 0x123;
 
-    sel = kzalloc(struct_size(sel, keys, 1), GFP_KERNEL);
-    if (!sel)
-        return -ENOMEM;
+	sel = kzalloc(struct_size(sel, keys, 1), GFP_KERNEL);
+	if (!sel)
+		return -ENOMEM;
 
-    cls_u32->knode.sel = sel;
+	cls_u32->knode.sel = sel;
 	sel->nkeys = 1;
 	sel->offshift = 0;
 	sel->keys[0].off = offset;
-	sel->keys[0].val = val << 16;
-	sel->keys[0].mask = 0xffff0000;
+	sel->keys[0].val = val;
+	sel->keys[0].mask = htonl(0xffff0000);
 
 	exts = kzalloc(sizeof(*exts), GFP_KERNEL);
 	if (!exts) {
@@ -1114,7 +1118,7 @@ static int stmmac_test_rxp_setup_new(
 		return -ENOMEM;
 	}
 
-    cls_u32->knode.exts = exts;
+	cls_u32->knode.exts = exts;
 	exts->actions = kcalloc(1, sizeof(struct tc_action *), GFP_KERNEL);
 	if (!exts->actions) {
 		pr_err("stmmac_test_rxp: Failed to allocate actions\n");
@@ -1127,10 +1131,10 @@ static int stmmac_test_rxp_setup_new(
 		pr_err("stmmac_test_rxp: Failed to allocate gact\n");
 		return -ENOMEM;
 	}
-    exts->actions[0] = (struct tc_action *)gact;
-    gact->tcf_action = tcfa;
+	exts->actions[0] = (struct tc_action *)gact;
+	gact->tcf_action = tcfa;
 
-    return 0;  /* Success */
+	return 0;  /* Success */
 }
 
 void stmmac_test_rxp_setup_delete(struct tc_cls_u32_offload *cls_u32)
@@ -1292,11 +1296,11 @@ static int stmmac_test_rxp2(struct stmmac_priv *priv)
 		return -EOPNOTSUPP;
 	}
 
-    ret = stmmac_test_rxp_setup_new(&cls_u32, htons(ETH_P_IP), 12, TC_ACT_SHOT);
-    if (ret) {
-        pr_err("stmmac_test_rxp: Failed to setup TC rule, ret=%d\n", ret);
-        return ret;
-    }
+	ret = stmmac_test_rxp_setup_new(&cls_u32, htonl(ETH_P_IP << 16), 12, TC_ACT_SHOT);
+		if (ret) {
+		pr_err("stmmac_test_rxp: Failed to setup TC rule, ret=%d\n", ret);
+		return ret;
+	}
 
 	pr_info("stmmac_test_rxp: Installing TC rule to drop packets from 0xdeadbeef\n");
 
@@ -1318,8 +1322,8 @@ static int stmmac_test_rxp2(struct stmmac_priv *priv)
 		pr_warn("stmmac_test_rxp: Packet was received (FAIL)\n");
 
 	/* Dump hardware stats after test */
-    pr_info("stmmac_test_rxp: Dumping FRP stats...\n");
-    dwmac5_frp_dump_stats(priv->ioaddr);
+	pr_info("stmmac_test_rxp: Dumping FRP stats...\n");
+	dwmac5_frp_dump_stats(priv->ioaddr);
 
 	ret = ret ? 0 : -EINVAL; /* Shall NOT receive packet */
 
@@ -2057,7 +2061,7 @@ static const struct stmmac_test {
 	// 	.fn = stmmac_test_dvlanfilt_perfect,
 	// },
 	{
-		.name = "Flexible RX Parser         ",
+		.name = "Flexible RX Parser2         ",
 		.lb = STMMAC_LOOPBACK_MAC,
 		.fn = stmmac_test_rxp2,
 	}, 
