2ef78c89447a4196d720d1d10a7753e40924eb45
Praveen Yaramada <praveen@axisbinc.com>
2025-11-12 23:56:23 -0800

    avb rx/tx pkt counters

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -284,6 +284,7 @@ struct stmmac_priv {
 	void *avb_data;
 	unsigned int avb_enabled;
 	unsigned int avb_rx_packets;
+	unsigned int avb_tx_packets;
 	//__ETHTOOL_DECLARE_LINK_MODE_MASK(phy_advertising);
     struct stmmac_avb_dma_conf *dma_avb_conf;
     raw_spinlock_t ptp_spinlock;
